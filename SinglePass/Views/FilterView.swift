9d5d2905217ccaf669500801daa0e3f668489016
wesley matlock <wmatlock@gmail.com>
2020-07-02 08:53:44 -0600

    Header and filter views.

--- SinglePass/Views/FilterView.swift
+++ SinglePass/Views/FilterView.swift
@@ -1,9 +1,33 @@
-//
-//  FilterView.swift
-//  SinglePass
-//
-//  Created by Matlock, Wesley on 7/2/20.
-//  Copyright © 2020 Liquidcoder. All rights reserved.
-//
+import SwiftUI
 
-import Foundation
+struct FilterView: View {
+  var filter: Filter
+  @Binding var isSelected: Bool
+  var onSelect: ((Filter) -> ()) = { _ in }
+
+  var body: some View {
+    ZStack(alignment: .bottom) {
+      Text(filter.rawValue)
+        .layoutPriority(1)
+        .foregroundColor(isSelected ? .text : .gray)
+        .padding(5)
+
+      if isSelected {
+        Rectangle()
+          .frame(width: 50, height: 3)
+          .foregroundColor(.accent)
+          .cornerRadius(2)
+          .transition(.scale)
+      }
+    }
+    .padding().onTapGesture {
+      onSelect(filter)
+    }
+  }
+}
+
+struct FilterView_Previews: PreviewProvider {
+  static var previews: some View {
+    FilterView(filter: .Passwords, isSelected: .constant(true))
+  }
+}
