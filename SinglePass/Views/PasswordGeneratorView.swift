923759d51098d3206bedd6ebdb6dfba61482c030
Wesley Matlock <168321+wesmatlock@users.noreply.github.com>
2020-07-06 20:12:46 -0600

    Created the password generator services and views. (#5)
    Co-authored-by: wesley matlock <wmatlock@gmail.com>

--- SinglePass/Views/PasswordGeneratorView.swift
+++ SinglePass/Views/PasswordGeneratorView.swift
@@ -4,12 +4,7 @@ struct PasswordGeneratorView: View {
 
   @Binding var generatedPassword: String
 
-  @State private var password = ""
-  @State private var lowercase = true
-  @State private var uppercase = false
-  @State private var specialCharacters = true
-  @State private var digits = false
-  @State private var length: CGFloat = 6
+  @ObservedObject private var passwordService = PasswordGeneratorService()
 
   var body: some View {
     VStack(spacing: 30) {
@@ -17,31 +12,37 @@ struct PasswordGeneratorView: View {
         Text("Generate password").font(.title).bold()
 
         Group {
-          SharedTextfield(value: self.$password, header: "Generated Password", placeholder: "Your new password", errorMessage: "", showUnderline: false, onEditingChanged: { flag in
+          SharedTextfield(value: $passwordService.password, header: "Generated Password", placeholder: "Your new password", errorMessage: "", showUnderline: false, onEditingChanged: { flag in
           }).padding()
         }.background(Color.background)
         .cornerRadius(10).neumorphic()
 
         VStack(spacing: 10){
-          Toggle(isOn: $lowercase) {
+          Toggle(isOn: $passwordService.options.lowercase) {
             Text("Lowercase")
           }.toggleStyle(CustomToggleStyle())
 
-          Toggle(isOn: $uppercase) {
+          Toggle(isOn: $passwordService.options.uppercase) {
             Text("Uppercase")
           }.toggleStyle(CustomToggleStyle())
 
-          Toggle(isOn: $specialCharacters) {
+          Toggle(isOn: $passwordService.options.specialCharacters) {
             Text("Special Characters")
           }.toggleStyle(CustomToggleStyle())
 
-          Toggle(isOn: $digits) {
+          Toggle(isOn: $passwordService.options.digits) {
             Text("Digits")
           }.toggleStyle(CustomToggleStyle())
 
-          Slider(value: $length, in: 1...30, step: 1)
+          HStack {
+            Slider(value: $passwordService.options.length, in: 1...30, step: 1)
             .accentColor(Color.accent)
 
+            Text("\(Int(passwordService.options.length))")
+              .font(.system(size: 20, weight: .bold))
+              .foregroundColor(.gray)
+              .frame(width: 30, height: 30)
+          }
         }.padding()
         .background(Color.background)
         .cornerRadius(10)
@@ -51,6 +52,8 @@ struct PasswordGeneratorView: View {
       .frame(maxHeight: .infinity)
       .background(Color.background)
       .edgesIgnoringSafeArea(.all)
+    }.onDisappear {
+      self.generatedPassword = self.passwordService.password
     }
   }
 }
